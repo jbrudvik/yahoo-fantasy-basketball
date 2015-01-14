ade9c86fc74536f7020e0d950053feac8fb39b52
Jeremy Brudvik <jeremy@brudvik.me>
2015-01-14 14:40:14 -0800

    Add notes and algorithm ideas

--- project-matchup.py
+++ project-matchup.py
@@ -1,3 +1,19 @@
 """
 Project results of the current matchup
+
+# Algorithm sketch
+- Loop through Mon-Sun, sucking in stats (add column for day)
+    - Mon: http://basketball.fantasysports.yahoo.com/nba/178276/6
+    - Tues: http://basketball.fantasysports.yahoo.com/nba/178276/6/team?&date=2015-01-13&stat1=S&stat2=D
+- Average / sum these
+- Also do for opponent
+- Output category statistics, and projected overall score vs opponent
+
+# Use
+- If anything major jumps out, address it through personnel changes
+
+# Implementation
+- numpy or pandas?
+- How to authenticate? Ideally, take environment vars
+    - Then set via $ YAHOO_USERNAME=foo YAHOO_PASSWORD=bar python project-matchup.py
 """
--- start-active-players.py
+++ start-active-players.py
@@ -1,3 +1,10 @@
 """
 Start active players for the week
+
+Ideas:
+- Include the names of players who cannot be started
+    - And maybe the full roster on those dates
+
+TODO:
+- Add required packages in requirements.txt
 """
