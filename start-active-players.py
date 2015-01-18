6a27bd99352e4dc7f38c6f819a8a45b37c1a094c
Jeremy Brudvik <jeremy@brudvik.me>
2015-01-18 13:58:41 -0800

    Remove TODO to add requirements.txt

--- start-active-players.py
+++ start-active-players.py
@@ -4,9 +4,6 @@ Start active players for the week
 Ideas:
 - Include the names of players who cannot be started
     - And maybe the full roster on those dates
-
-TODO:
-- Add required packages in requirements.txt
 """
 
 import requests
