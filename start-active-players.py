b2a0adee470c32e96c91f4b697c3555911d7b477
Jeremy Brudvik <jeremy@brudvik.me>
2015-01-18 13:54:22 -0800

    Begin login

--- start-active-players.py
+++ start-active-players.py
@@ -8,3 +8,18 @@ Ideas:
 TODO:
 - Add required packages in requirements.txt
 """
+
+import requests
+from bs4 import BeautifulSoup
+
+# TODO: Configure this somewhere better (as a direct argument to the script, probably
+TEAM_URL = 'http://basketball.fantasysports.yahoo.com/nba/178276/6/'
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36'
+}
+response = requests.get(TEAM_URL, headers=headers)
+soup = BeautifulSoup(response.text)
+inputs = soup.find(id='hiddens').findAll('input')
+fields = {input['name']: input['value'] for input in inputs}
+print(fields)
